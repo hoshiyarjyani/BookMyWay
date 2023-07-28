00562f4c3d3501784fd80ef429d2696a02e5751c
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-07-28 23:38:28 +0530

    Dependency added sucessfull

---
+++ BookMyWay/src/main/java/com/bookmyway/BookMyWayApplication.java
@@ -0,0 +1,13 @@
+package com.bookmyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyWayApplication.class, args);
+	}
+
+}
