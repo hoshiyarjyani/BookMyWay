6c7c1462d116fab678320968720c351b4ab0c3c5
Shahid Afridi <siddiquibabuddin202@gmail.com>
2023-08-09 10:36:26 +0530

    Implement  with Exception

--- BookMyWay/src/main/java/com/bookmyway/exception/GlobalExceptionHandler.java
+++ BookMyWay/src/main/java/com/bookmyway/exception/GlobalExceptionHandler.java
@@ -42,7 +42,54 @@ public class GlobalExceptionHandler {
 		return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
 	}
 
- 
-	 
+
+	@ExceptionHandler(NotFoundException.class)
+	public ResponseEntity<MyErrorDetails> exceptionHandler2(NotFoundException ce, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails();
+		err.setTimpstamp(LocalDateTime.now());
+		err.setMessage(ce.getMessage());
+		err.setDetails(req.getDescription(false));
+
+		return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(BusException.class)
+	public ResponseEntity<MyErrorDetails> exceptionHandler2(BusException ce, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails();
+		err.setTimpstamp(LocalDateTime.now());
+		err.setMessage(ce.getMessage());
+		err.setDetails(req.getDescription(false));
+
+		return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+	}
+	@ExceptionHandler(TrainException.class)
+	public ResponseEntity<MyErrorDetails> exceptionHandler2(TrainException ce, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails();
+		err.setTimpstamp(LocalDateTime.now());
+		err.setMessage(ce.getMessage());
+		err.setDetails(req.getDescription(false));
+		
+		return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+	}
+	@ExceptionHandler(HotelException.class)
+	public ResponseEntity<MyErrorDetails> exceptionHandler2(HotelException ce, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails();
+		err.setTimpstamp(LocalDateTime.now());
+		err.setMessage(ce.getMessage());
+		err.setDetails(req.getDescription(false));
+		
+		return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+	}
+	@ExceptionHandler(FlightException.class)
+	public ResponseEntity<MyErrorDetails> exceptionHandler2(FlightException ce, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails();
+		err.setTimpstamp(LocalDateTime.now());
+		err.setMessage(ce.getMessage());
+		err.setDetails(req.getDescription(false));
+		
+		return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+	}
+
+
 
 }
